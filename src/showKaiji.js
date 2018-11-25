623276f59762559ab88bf303a315cb3e407ae62f
Nokit <stmarc.tn@gmail.com>
2018-11-25 17:28:25 +0900

    let it know the filepath

--- src/showKaiji.js
+++ src/showKaiji.js
@@ -2,8 +2,8 @@
     'use strict';
     function ShowTime(){
 	var now = new Date();
-	var fileName = "Tdnet-rss-"+now.getFullYear().toString()+(now.getMonth()+1).toString()+now.getDate().toString()+".xml" 
+	var fileName = "getTdnet/Tdnet-rss/Tdnet-rss-"+now.getFullYear().toString()+(now.getMonth()+1).toString()+now.getDate().toString()+".xml"; 
 	document.getElementById('Data').innerText = fileName;	
     }
-    setInterval(ShowTime, 50);
+	setInterval(ShowTime, 50);
 })();
