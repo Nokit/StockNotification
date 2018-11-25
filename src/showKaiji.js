30e2a12d0fde1e9e396906d2dc9ff2056be83732
Nokit <stmarc.tn@gmail.com>
2018-11-25 16:56:15 +0900

    file Name

--- src/showKaiji.js
+++ src/showKaiji.js
@@ -1,8 +1,9 @@
 (function(){
     'use strict';
     function ShowTime(){
-	 var now = new Date();
-	 document.getElementById('Data').innerText = now.getFullYear().toString()+(now.getMonth()+1).toString()+now.getDate().toString();
+	var now = new Date();
+	var fileName = "Tdnet-rss-"+now.getFullYear().toString()+(now.getMonth()+1).toString()+now.getDate().toString()+".xml" 
+	document.getElementById('Data').innerText = fileName;	
     }
     setInterval(ShowTime, 50);
 })();
