cc844c144a5d539491b388afce0162bb94b95c2f
Nokit <stmarc.tn@gmail.com>
2018-11-25 15:58:56 +0900

    Refactoring

--- load.py
+++
@@ -1,9 +0,0 @@
-from edinet_xbrl.edinet_xbrl_downloader import EdinetXbrlDownloader
-
-## init downloader
-xbrl_downloader = EdinetXbrlDownloader()
-
-## set a ticker you want to download xbrl file
-ticker = "1234"
-target_dir = "/"
-xbrl_downloader.download_by_ticker(ticker, target_dir)
